c762caebbf1e6221b7f53a6859cd14b368b9a0e5
PariGupta2007 <75723318+PariGupta2007@users.noreply.github.com>
2020-12-27 19:25:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+var car, wall;
+var bullet, speed, weight, thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+speed=random(55,90)
+weight=random(400,1500)
+
+thickness=random(22,83)
+
+bullet=createSprite(50, 200, 50, 5);
+bullet.velocityX = speed;
+bullet.shapeColor=color(225);
+
+wall=createSprite(1200, 200, thickness, height/2)
+
+wall.shapeColor=color(80,80,80)
+}
+
+function draw() {
+  background(0);  
+ if(wall.x-car.x<(car.width+wall.width)/2)
+ {
+   car.velocityX=0;
+   var deformation=0.5*weight*speed*speed/22509;
+   if(deformation>180)
+   {
+   car.shapeColor=color(255,0,0);
+   }
+   if(deformation<180 && deformation>100)
+   {
+   car.shapeColor=color(230,230,0);
+   }
+   if(deformation<100)
+   {
+    car.shapeColor=color(0,255,0);
+   }
+ }
+}
+function draw() {
+  background(0,0,0);  
+  
+if(hc(bullet,wall))
+{
+bullet.velocityX=0;
+var d=0.5 * weight * speed* speed/(thickness *thickness *thickness)
+
+if(d>10)
+{
+wall.shapeColor=color(255,0,0);
+}
+if(d<10)
+{
+wall.shapeColor=color(0,255,0);
+}
+}
+  drawSprites();
+}
+
+
+function hc(Ibullet,Iwall){
+
+bulletRightEdge=Ibullet.x +Ibullet.width;
+wallLeftEdge=Iwall.x;
+
+
+if(bulletRightEdge>=wallLeftEdge){
+return true 
+}
+return false;
+}
